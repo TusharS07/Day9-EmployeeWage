751fa42f03bd7074d4049b242ae007f79b03a525
Salunkhe@07 <salunkhet07@gmail.com>
2022-09-30 23:33:06 +0530

    Emp Wage Compute Using Oop

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,43 +1,49 @@
-package com.Bridgelabz.EmployeeWageBuilder;
+package com.Bridgelabz.EmployeeWageDay9;
 
-public class empWageBuilder {
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static int empPerHR_wage = 20;
+public class EmpWageBuilder {
+    public final int FULL_TIME = 1;
+    public final int PART_TIME = 2;
+    public int empPerHR_wage = 20;
 
-    //emplyee hours
 
-    static int num_of_Working_days = 20;
-    static int max_Hour_In_Month = 100;
-    static int empHrs = 0;
-    static int totalEmpHr = 0;
-    static int totalWorkingDays = 0;
+    public int num_of_Working_days = 20;
+    public int max_Hour_In_Month = 100;
+
 
     public static void main(String[] args) {
-        getEmpTotalWage();
+        TotalEmpWage totalWage = new TotalEmpWage();
+        totalWage.getEmpTotalWage();
+
 
     }
 
-    static void getEmpTotalWage() {
-        while (totalEmpHr <= max_Hour_In_Month && totalWorkingDays < num_of_Working_days) {
-            totalWorkingDays++;
-            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-            switch (empCheck) {
-                case FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
+    public static class  TotalEmpWage extends EmpWageBuilder {
+
+        public int empHrs = 0;
+        public int totalEmpHr = 0;
+        public int totalWorkingDays = 0;
+
+
+        public void getEmpTotalWage() {
+            while (totalEmpHr <= max_Hour_In_Month && totalWorkingDays < num_of_Working_days) {
+                totalWorkingDays++;
+                int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+                switch (empCheck) {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                }
+                totalEmpHr += empHrs;
+                System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
             }
-            totalEmpHr += empHrs;
-            System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
+            int totalEmpWage = totalEmpHr * empPerHR_wage;
+            System.out.println("Employee Total Wage: " + totalEmpWage + " Rs");
+
         }
-        int totalEmpWage = totalEmpHr * empPerHR_wage;
-        System.out.println("Employee Total Wage: " +totalEmpWage+ " Rs");
-
     }
-
 }
