dbdcf4b031ec2f1e19d431cf1e14c95419afd491
Salunkhe@07 <salunkhet07@gmail.com>
2022-09-30 21:31:45 +0530

    Check Employee is Present or Absent

---
+++ empWageBuilder.java
@@ -0,0 +1,22 @@
+package com.Bridgelabz.EmployeeWageBuilder;
+
+public class empWageBuilder {
+    static int FULL_TIME = 1;
+
+    public static void main(String[] args) {
+        //check emp present or not
+        checkEmpPresent();
+
+
+    }
+
+    static double checkEmpPresent() {
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Present");
+
+        return empCheck;
+    }
+}
